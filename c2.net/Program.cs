b9eece9f3f550233c92b63b12d7a768b1861889c
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-23 10:14:49 +0100

    Agregar archivos de proyecto.

---
+++ c2.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c2.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
