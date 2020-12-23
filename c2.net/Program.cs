8fdee780be66ee4ecc514eac739e395fe5f2c7e9
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-23 10:34:54 +0100

    solucion_01

--- c2.net/Program.cs
+++ c2.net/Program.cs
@@ -6,8 +6,21 @@ namespace c2.net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declara dos variables numéricas(con el valor que desees), muestra por consola la suma,
+            //resta, multiplicación, división y módulo(resto de la división).
+            int a = 7;
+            int b = 9;
 
+            //suma
+            Console.WriteLine(a + b);
+            //resta
+            Console.WriteLine(a - b);
+            //multiplicacion
+            Console.WriteLine(a * b);
+            //dividion
+            Console.WriteLine(a / b);
+            //residu
+            Console.WriteLine(a % b);
         }
     }
 }
