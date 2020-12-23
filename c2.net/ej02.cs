b3d0aca66978fbeeacfcffe641ed6896a898c780
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-23 10:58:10 +0100

    solucion

--- c2.net/ej02.cs
+++ c2.net/ej02.cs
@@ -6,5 +6,25 @@ namespace c2.net
 {
     class ej02
     {
+        static void Main(string[] args)
+        {
+            //Declara dos variables numéricas(con el valor que desees), muestra por consola la suma,
+            //resta, multiplicación, división y módulo(resto de la división).
+            int a = 7;
+            int b = 9;
+
+            //suma
+            Console.WriteLine(a + b);
+            //resta
+            Console.WriteLine(a - b);
+            //multiplicacion
+            Console.WriteLine(a * b);
+            //dividion
+            Console.WriteLine(a / b);
+            //residu
+            Console.WriteLine(a % b);
+
+            Console.ReadKey();
+        }
     }
 }
