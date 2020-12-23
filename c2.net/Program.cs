aa0a833f17ba7a268a7b80e6c493d4504f8654ce
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-23 11:54:01 +0100

    solucion final

--- c2.net/Program.cs
+++ c2.net/Program.cs
@@ -6,7 +6,22 @@ namespace c2.net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //5 - Programa de consola que declare cuatro variables enteras A, B, C
+            // y D y asígnale un valor a cada una.A continuación realiza las
+            // instrucciones necesarias para que:
+
+            int a = 3, b = 5, c = 4, d = 7;
+
+            b = c;
+            c = a;
+            a = d;
+            d = b;
+
+            Console.WriteLine("B toma el valor de C -> B = " + b);
+            Console.WriteLine("C toma el valor de A -> C = " + c);
+            Console.WriteLine("A toma el valor de D -> A = " + a);
+            Console.WriteLine("D toma el valor de B -> D = " + d);
+
 
         }
     }
