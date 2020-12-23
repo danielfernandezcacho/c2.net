bda63b3ccfb68d2b1ef273d3de43e609ca36cd03
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-23 11:21:24 +0100

    solucion final

--- c2.net/Program.cs
+++ c2.net/Program.cs
@@ -6,8 +6,31 @@ namespace c2.net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            int X = 20, Y = 5;
+            double M = 6.1, N = 3.2;
+            Console.WriteLine("Variable X = " + X);
+            Console.WriteLine("Variable Y = " + Y);
+            Console.WriteLine("Variable M = " + M);
+            Console.WriteLine("Variable N = " + N);
+            Console.WriteLine(X + " + " + Y + " = " + (X + Y));
+            Console.WriteLine(X + " - " + Y + " = " + (X - Y));
+            Console.WriteLine(X + " * " + Y + " = " + X * Y);
+            Console.WriteLine(X + " / " + Y + " = " + X / Y);
+            Console.WriteLine(X + " % " + Y + " = " + X % Y);
+            Console.WriteLine(N + " + " + M + " = " + (N + M));
+            Console.WriteLine(N + " - " + M + " = " + (N - M));
+            Console.WriteLine(N + " * " + M + " = " + N * M);
+            Console.WriteLine(N + " / " + M + " = " + N / M);
+            Console.WriteLine(N + " % " + M + " = " + N % M);
+            Console.WriteLine(X + " + " + N + " = " + (X + N));
+            Console.WriteLine(Y + " / " + M + " = " + Y / M);
+            Console.WriteLine(Y + " % " + M + " = " + Y % M);
+            Console.WriteLine("Variable X = " + X + " el doble es " + 2 * X);
+            Console.WriteLine("Variable Y = " + Y + " el doble es " + 2 * Y);
+            Console.WriteLine("Variable M = " + M + " el doble es " + 2 * M);
+            Console.WriteLine("Variable N = " + N + " el doble es " + 2 * N);
+            Console.WriteLine(X + " + " + Y + " + " + N + " + " + M + " = " + (X + Y + M + N));
+            Console.WriteLine(X + " * " + Y + " * " + N + " * " + M + " = " + (X * Y * M * N));
         }
     }
 }
